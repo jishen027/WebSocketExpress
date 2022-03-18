59cd110dd851c3268434e57e0193a5a21d320c02
JipuLi <jishen027@gmail.com>
2022-03-18 01:24:26 +0000

    [ADD] init realtime chat

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 
 
@@ -12,9 +13,32 @@ const io = socketio(server)
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+const botName = 'chat bot'
+
 //run when client connects
-io.on('connection', socket=>{
-  console.log('New client connected')
+io.on('connection', socket => {
+
+  socket.on('joinRoom', ({ username, room }) => {
+    // single clients
+    socket.emit('message', formatMessage(botName, "welcome to the chat room"))
+
+    // all clients except the  connecting one
+    socket.broadcast.emit('message', formatMessage(botName, 'a user joined chat'))
+
+    // all clients
+    // io.emit()
+
+    // Run when client disconnects
+    socket.on('disconnect', () => {
+      io.emit('message', formatMessage(botName, 'A user has left the chat'))
+    })
+
+  })
+
+  // Listen for chat message
+  socket.on('chatMessage', msg => {
+    io.emit('message', formatMessage('USRE', msg))
+  })
 })
 
 const PORT = 3000 || process.env.PORT
